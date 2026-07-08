8a5aab86e20ba412418bc3ce24b1db30ddd234b9
mohamed <maal43983@gmail.com>
2026-07-08 22:40:27 +0300

    first change

--- Frontend/src/components/landingPage.js
+++ Frontend/src/components/landingPage.js
@@ -1,3 +1,21 @@
+//loops
+//for loop
 
+for(i=0;i<=5;i++){
+    console.log("hala madrid")
+}
 
-conso.log("shhssh");
+//while loop
+let x=1;
+while(x<=4){
+    console.log(x)
+    i++
+}
+
+//do while
+
+let y=1
+do{
+    console.log(y)
+    i++
+}while(y<=4)
